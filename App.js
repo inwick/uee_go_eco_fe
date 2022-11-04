36e49d9949786693dee8722754ba51ffaa21ad72
kushaniSakalasooriya <79516521+kushaniSakalasooriya@users.noreply.github.com>
2022-11-04 11:32:54 +0530

    food waste server initial commit

--- App.js
+++ App.js
@@ -6,15 +6,24 @@ import FuelSavingTips from './src/screens/fuel_saver/FuelSavingTips';
 import FuelCostCalculator from './src/screens/fuel_saver/FuelCostCalculator';
 import FuelEfficiencyCalculator from './src/screens/fuel_saver/FuelEfficiencyCalculator';
 
+import FoodSaverDashboard from './src/screens/food_saver/FoodSaverDashboard';
+
 const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="StartScreen">
+
+        {/* fuel saver */}
         <Stack.Screen name="FuelSaverDashBoard" component={FuelSaverDashBoard} options={{ title: 'Fuel Saver DashBoard' }} />
         <Stack.Screen name="FuelSavingTips" component={FuelSavingTips} options={{ title: 'Fuel Saving Tips' }} />
         <Stack.Screen name="FuelCostCalculator" component={FuelCostCalculator} options={{ title: 'Fuel Cost Calculator' }} />
         <Stack.Screen name="FuelEfficiencyCalculator" component={FuelEfficiencyCalculator} options={{ title: 'Fuel Efficiency Calculator' }} />
+
+
+        {/* food waste saver */}
+        <Stack.Screen name="FoodSaverDashboard" component={FoodSaverDashboard} options={{ title: 'Food  Waste Reduce DashBoard' }} />
+
       </Stack.Navigator>
 
     </NavigationContainer>
---
+++ src/screens/food_saver/FoodSaverDashboard.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, TouchableOpacity, Text, Image } from "react-native";
+import axios from 'react-native-axios';
+import { useNavigation } from '@react-navigation/native';
+
+function FoodSaverDashboard() {
+
+    const [wallets, setWallets] = useState([]);
+    const navigation = useNavigation();
+
+    const getWalletItems = async () => {
+        try {
+            const response = await axios.get(`http://192.168.1.111:5050/wallet`);
+            setWallets(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        getWalletItems();
+    }, [])
+
+    return (
+
+        <View style={styles.MainContainer}>
+
+            <Text style={{
+                fontSize: 30,
+                fontWeight: 'bold',
+                color: 'black'
+            }}>
+                Food Waste Reducing
+            </Text>
+            <Image source={require('../../assets/food_waste_saver/food1.png')} style={styles.img} />
+
+            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("AddFoodSavingTips")}>
+                <Text style={styles.text}>Add Food Waste Reducing Tips</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("FoodSavingTips")}>
+                <Text style={styles.text}>Food Waste Reducing Tips</Text>
+            </TouchableOpacity>
+{/* 
+            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("FuelEfficiencyCalculator")}>
+                <Text style={styles.text}>Fuel Efficiency Calculator</Text>
+            </TouchableOpacity> */}
+
+        </View>
+
+    );
+}
+export default FoodSaverDashboard
+
+const styles = StyleSheet.create({
+    MainContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 20
+    },
+
+    button: {
+        width: '80%',
+        paddingTop: 2,
+        paddingBottom: 2,
+        backgroundColor: '#ffc107',
+        borderRadius: 20,
+        marginTop: 20,
+        height: 100
+    },
+    text: {
+        color: '#000',
+        fontSize: 20,
+        textAlign: 'center',
+        padding: 30,
+        fontWeight: "500"
+    },
+
+    img: {
+        width: 150,
+        height: 150,
+        marginBottom: 5,
+        marginTop: 10
+    },
+
+});
