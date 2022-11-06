e36ac955cabcaf2dd6d9c6bb76dee04fd296f771
inwick <indikasandun2015@gmail.com>
2022-11-06 18:33:56 +0530

    Fuel saver Bug fixed 1.0

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import FuelSaverDashBoard from './src/screens/fuel_saver/FuelSaverDashBoard';
 import FuelSavingTips from './src/screens/fuel_saver/FuelSavingTips';
 import FuelCostCalculator from './src/screens/fuel_saver/FuelCostCalculator';
 import FuelEfficiencyCalculator from './src/screens/fuel_saver/FuelEfficiencyCalculator';
+import FuelTipView from './src/screens/fuel_saver/FuelTipView';
 
 const Stack = createNativeStackNavigator();
 const App = () => {
@@ -17,6 +18,7 @@ const App = () => {
         <Stack.Screen name="FuelSavingTips" component={FuelSavingTips} options={{ title: 'Fuel Saving Tips' }} />
         <Stack.Screen name="FuelCostCalculator" component={FuelCostCalculator} options={{ title: 'Fuel Cost Calculator' }} />
         <Stack.Screen name="FuelEfficiencyCalculator" component={FuelEfficiencyCalculator} options={{ title: 'Fuel Efficiency Calculator' }} />
+        <Stack.Screen name="FuelTipView" component={FuelTipView} options={{ title: 'Tip View' }} />
       </Stack.Navigator>
 
     </NavigationContainer>
